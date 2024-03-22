8d272894b2653a600e384a23ba02d036fdc331f3
omkar nath dubey <omkardubey1530@gmail.com>
2024-03-22 20:41:28 +0530

    commit it

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <h1 className='text-3xl font-bold font-mono'>Hello hanuman ji</h1>
+   </div>
+  )
+}
+
+export default App
